f4629f7d381490ca8caf5b4c2b389c9e137e0024
David Chen <45245358+TheEgghead27@users.noreply.github.com>
2021-10-17 11:39:22 -0400

    Add initial WordPair function, begin RandomWords widger

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -9,16 +10,39 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
   }
 }
+
+
+// stateless = immutable; stateful = mutable
+// stateful needs StatefulWidget (immutable), which makes State (mutable, persistent)
+// stful to general boilerplate
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+// state of class is where logic and stuff goes
+class _RandomWordsState extends State<RandomWords> {  // underscore is privacy indicator, best practice
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+    // return Container();  generic
+  }
+}
+
