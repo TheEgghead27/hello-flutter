c4cc68c566f07fb7a2649d4ed009591703b30919
David Chen <45245358+TheEgghead27@users.noreply.github.com>
2021-10-17 12:10:45 -0400

    Add infinite scroll stuff

--- lib/main.dart
+++ lib/main.dart
@@ -12,19 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          // child: Text(wordPair.asPascalCase),
-          child: RandomWords(),  // we can use the widget as a replacement now
-        ),
-      ),
+      home: RandomWords(),  // app bar and body defined here now
     );
   }
 }
-
 
 // stateless = immutable; stateful = mutable
 // stateful needs StatefulWidget (immutable), which makes State (mutable, persistent)
@@ -37,12 +28,50 @@ class RandomWords extends StatefulWidget {
 }
 
 // state of class is where logic and stuff goes
-class _RandomWordsState extends State<RandomWords> {  // underscore is privacy indicator, best practice
+class _RandomWordsState extends State<RandomWords> {
+  // underscore is privacy indicator, best practice
+
+  final _suggestions = <WordPair>[]; // array of wordpairs
+  final _biggerFont = const TextStyle(fontSize: 18.0); // wowie wow wow fnots
+  final wordPair = WordPair.random();
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        // make list
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          // builder property that does the building, called once per pair
+          if (i.isOdd)
+            return const Divider(); /*2, make a divider thing before each row */
+
+          final index =
+              i ~/ 2; /*3, integer division for calculating the number of rows*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs()
+                .take(10)); /*4, make more rows if we ran out*/
+          }
+          return _buildRow(_suggestions[index]); // return a word block
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(  // define title and table structure stuff in this widget
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+    // return Text(wordPair.asPascalCase);
     // return Container();  generic
   }
 }
-
